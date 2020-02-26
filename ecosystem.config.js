b39fb9b049725c3618e06d71bbe0e066eb3e0d89
Carlos Wu <carlos.fei@canonical.com>
2020-02-26 22:18:06 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,6 @@ module.exports = {
     name: 'docker-demo',
     // Remove all docker containers using same image and start new container
     script: 'docker rm -f $(docker ps -aq --filter ancestor=docker-demo) && docker build --tag docker-demo . && docker run -p 5000:80 -d docker-demo',
-   
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    //args: '--interpreter bash',
-    //exec_interpreter: '~/miniconda/envs/test1/bin/python',
     instances: 1,
     autorestart: false,
     watch: false,
@@ -15,11 +11,9 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'root',
-      host : '212.83.163.1',
       ref  : 'origin/master',
-      //repo : 'carkod@github.com:binboard.git',
-      path : '/var/www/binboard',
+      repo : 'carkod@github.com:docker-demo.git',
+      path : '/var/www/docker-demo',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production'
     }
   }
