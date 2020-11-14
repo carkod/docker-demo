64cde60762cb6f6ebb5bdcab4e0f2a70aa29586a
Carlos Wu Fei <carlos.fei@canonical.com>
2020-11-14 20:36:51 +0000

    Test .env content

--- api/submodule/submodule.py
+++ api/submodule/submodule.py
@@ -1,5 +1,8 @@
+import os
 from api.sibling.sibling import sibling
+
+secret = os.getenv("SECRET_KEY")
 
 def submodule():
     text = sibling()
-    return f"This is a submodule! {text}"
+    return f"This is a submodule! {text} and here is a env file var {secret}"
